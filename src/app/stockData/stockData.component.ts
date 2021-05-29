cdc8434d3b58793be19edfd84ed63b83b5ce5abe
Himanshu Dewangan <himanshu.dewangan@yahoo.com>
2021-05-29 12:00:26 +0530

    api call logic and data logic

--- src/app/stockData/stockData.component.ts
+++ src/app/stockData/stockData.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'stock-data',
@@ -7,7 +8,26 @@ import {Component, OnInit} from '@angular/core';
 })
 export class StockData implements OnInit {
 
+  constructor(private http: HttpClient) {
+  }
   ngOnInit() {
     
+  }
+
+  inputDate: String;
+  responseData: any;
+  noResultFound: Boolean = false;
+
+  getData(){
+    let url = 'https://jsonmock.hackerrank.com/api/stocks?date=' + this.inputDate;
+    this.http.get(url).subscribe((response: any) => {
+      if(response.data.length > 0){
+        this.responseData = response.data[0];
+        this.noResultFound = false;
+      }else{
+        this.responseData = null;
+        this.noResultFound = true;
+      }
+    });
   }
 }
